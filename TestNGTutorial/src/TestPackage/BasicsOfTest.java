ef43ad500b53691cfa7ccb68416532e68f9b500d
Khateejapasha <khateejapasha11@gmail.com>
2025-08-30 22:33:53 +0530

    American guy commit

--- TestNGTutorial/src/TestPackage/BasicsOfTest.java
+++ TestNGTutorial/src/TestPackage/BasicsOfTest.java
@@ -10,6 +10,10 @@ public void sampleMethod()
 	System.out.println("Hello i am git1");
 	System.out.println("Hello i am git2");
 	System.out.println("Hello i am git3");
+	System.out.println("Hello i am git4");
+	System.out.println("Hello i am git5");
+	System.out.println("Hello i am git6");
+	System.out.println("Hello i am git7");
 }
 
 
